0b8569d1533ca46dcf43d1a7099ad9717fcead51
Daniel Danis <daniel.gordon.danis@protonmail.com>
2024-07-25 11:50:43 +0200

    Add deprecation note.

--- src/ppktstore/archive/__init__.py
+++ src/ppktstore/archive/__init__.py
@@ -1,5 +1,7 @@
 
 from ._archiver import PhenopacketStoreArchiver
+
+# TODO: these should be removed at some point.
 # from .cohort import Cohort
 # from .cohort_extractor import CohortExtractor
 # from .ppacket import PPacket
